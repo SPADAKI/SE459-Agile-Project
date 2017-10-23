756671e4ec3cacb72964faca4fdcd3ec548b2efb
Ming-Kang <32498937+cw268532@users.noreply.github.com>
2017-10-23 13:24:25 -0500

    two Answerboard popup window

---
+++ AgileGame/src/game/CorrectAnswerBoard.java
@@ -0,0 +1,87 @@
+package game;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+public class CorrectAnswerBoard {
+	
+	private JFrame rightans;
+	private JPanel p;
+	private JButton exit;
+	
+	
+	
+	public CorrectAnswerBoard()
+	{
+		makeinterface();
+	}
+	
+	public void makeinterface() {
+		rightans = new JFrame("you got right ans");
+		rightans.setVisible(true);
+		rightans.setSize(750, 200);
+		rightans.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		p = new JPanel();
+		
+		JLabel correctmessage = new JLabel("you got the correct answer",JLabel.CENTER);
+		
+		exit = new JButton("exit"); 
+		exit.setSize(250, 100);
+		TeamHandler t1handler = new TeamHandler();
+		exit.addMouseListener(t1handler);
+		
+		
+		
+		p.add(exit);
+		rightans.add(p,BorderLayout.SOUTH);
+		rightans.add(correctmessage,BorderLayout.CENTER);
+	}
+	
+	private class TeamHandler implements MouseListener, MouseMotionListener{
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			rightans.dispose();					
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
+}
