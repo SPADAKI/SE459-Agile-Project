9020df33ba14eb511167a1dbd7121427b0cffc78
rmacwan2 <macwanr278@dupage.edu>
2017-10-29 12:26:56 -0500

    Added QuestionCard

---
+++ AgileGame/src/game/QuestionCard.java
@@ -0,0 +1,20 @@
+package game;
+
+/**
+ * Created by Ryan on 10/26/2017.
+ */
+public class QuestionCard {
+    private static int minWidth = 100;
+    private static int minHeight = 100;
+    private static int width = 900;
+    private static int height = 300;
+
+    public static int getMinWidth() { return minWidth; }
+
+    public static int getMinHeight() { return minHeight; }
+
+    public static int getWidth() { return width; }
+
+    public static int getHeight() { return height; }
+
+}
