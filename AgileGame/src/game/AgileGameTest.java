39989a99958d2810112e2a07579a5e4228ad6f4d
SPADAKI <padakishardul@gmail.com>
2017-11-04 12:59:22 -0500

    Added Junit Test

---
+++ AgileGame/src/game/AgileGameTest.java
@@ -0,0 +1,14 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AgileGameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
