4619ccf3b6872cc69ddcac003874a15456cefb7a
SPADAKI <padakishardul@gmail.com>
2017-11-11 16:25:14 -0600

    Unit Test Cases and Mockito

---
+++ AgileGame/src/game/LandingPageTest.java
@@ -0,0 +1,28 @@
+package game;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LandingPageTest {
+
+	@Test
+	public final void testLandingPage() {
+		System.out.println("Landing Page Loading......"); // TODO
+	}
+
+	@Test
+	public final void testAGPage() {
+		System.out.println("Loading AGPage......"); // TODO
+	}
+
+	@Test
+	public final void testGetScene() {
+		System.out.println("Getting Scene......"); // TODO
+	}
+
+	@Test
+	public final void testUpdateSize() {
+		System.out.println("Updating Size......");// TODO
+	}
+
+}
