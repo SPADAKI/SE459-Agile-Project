684d4d373a44e9aa6fa73da8125cfa2fa1abe16a
Ming-Kang <32498937+cw268532@users.noreply.github.com>
2017-11-13 13:18:32 -0600

    test update

---
+++ AgileGame/src/UnitTest/AgileGameTest.java
@@ -0,0 +1,59 @@
+package UnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AgileGameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStartStage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowLandingPage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowCongratulationPopup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHeight() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ AgileGame/src/UnitTest/TeamTest.java
@@ -0,0 +1,70 @@
+ package UnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import exception.DuplicatePlayerException;
+import exception.NullPlayerException;
+import game.Player;
+import game.Team;
+
+public class TeamTest {
+	Team t1 = new Team("test1");
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTeam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTeamName() {
+		t1.setTeamName("change");
+		assertEquals(t1.getTeamName(),"change");
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTeamName() {
+		Team t2 = new Team("test2");
+		assertEquals(t2.getTeamName(),"test2");
+	}
+
+	@Test
+	public void testGetScore() {
+		assertEquals(t1.getScore(),0);
+	}
+
+	@Test
+	public void testClearScore() {
+		t1.addPoints(5);
+		t1.clearScore();
+		assertEquals(t1.getScore(),0);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPoints() {
+		t1.addPoints(5);
+		assertEquals(t1.getScore(),5);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMember() throws DuplicatePlayerException, NullPlayerException {
+		Player pp = new Player("pp");
+		t1.addMember(pp);
+		fail("Not yet implemented");
+	}
+
+}
